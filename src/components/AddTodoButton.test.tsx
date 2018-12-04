c425cc8dc610ab225dbea0808208a43a55563927
butachin <b1016094@fun.ac.jp>
2018-12-04 17:09:15 +0900

    add: AddTodoButton.test.tsx

---
+++ src/components/AddTodoButton.test.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import AddTodoButton from './AddTodoButton';
+
+import * as Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
+
+it("renders input", () => {
+  let res: string;
+  const onDummy = (s: string) => {return;};
+  const wrapper = enzyme.shallow(<AddTodoButton onSubmit={onDummy}/>);
+  wrapper.setState({input: "hello"});
+  expect(wrapper.find("button").text()).toEqual("Add Todo");
+});
